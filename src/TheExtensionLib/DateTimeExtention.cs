3750dbc1565a8e8977f5cde56e6439333f88197a
Lukas Winzenried <lukas.winzenried@netiva.ch>
2014-02-19 13:38:36 +0100

    add datetime trim extension

---
+++ src/TheExtensionLib/DateTimeExtention.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace System
+{
+    public static class DateTimeExtention
+    {
+        public static DateTime Trim(this DateTime date, long roundTicks)
+        {
+            return new DateTime(date.Ticks - date.Ticks % roundTicks);
+        }
+    }
+}
