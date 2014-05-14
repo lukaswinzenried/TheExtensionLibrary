8cc3f2d843a033e5ad9ec5b932101fe4bc0ebb24
Lukas Winzenried <lukas@netiva.ch>
2014-05-14 12:43:49 +0200

    add array extension - rotate

---
+++ src/TheExtensionLib/ArrayExtension.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace System
+{
+    public static class ArrayExtensions
+    {
+        public static T[] Rotate<T>(this T[] source)
+        {
+            var rotated = new T[source.Length];
+            rotated[source.Length - 1] = source[0];
+            Array.Copy(source, 1, rotated, 0, source.Length - 1);
+            return rotated;
+        }
+    }
+}
